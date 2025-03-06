84797969bdaf6fa8d91fa15b8100829d7d1a61de
Chong Yong Xuan <103051564+CYX0410@users.noreply.github.com>
2025-03-06 11:49:22 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { environment } from '../environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
